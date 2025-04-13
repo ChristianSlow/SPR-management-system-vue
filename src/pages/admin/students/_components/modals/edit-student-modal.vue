a6d515f79a99b955d31ffbed7117c255aeaf4b62
ChristianSlow <christianslow.cpsu@gmail.com>
2025-04-13 17:53:26 +0800

    enhance student modal

--- src/pages/admin/students/_components/modals/edit-student-modal.vue
+++ src/pages/admin/students/_components/modals/edit-student-modal.vue
@@ -1,25 +1,25 @@
 <script setup lang="ts">
-import { useSubjectStore } from '@/stores/subject'
-import type { Subject } from '@/types/subject'
-import { useToast } from 'primevue/usetoast'
+import StudentLayout from '@/layouts/student-layout.vue'
+import { useStudentStore } from '@/stores/student'
+import type { Student } from '@/types/student'
+import { Message, useToast } from 'primevue'
 import { inject, reactive } from 'vue'
 
 const dialogRef = inject<any>('dialogRef')
-const subject = reactive<Subject>({ ...dialogRef.value.data }) // Preload subject for editing
-
-const store = useSubjectStore()
+const student = reactive<Student>({})
+const store = useStudentStore()
 const toast = useToast()
 
 function onClose() {
   dialogRef.value.close()
 }
 
-function onSubmit(payload: Subject) {
-  store.editSubject(payload)
+function onSubmit(payload: Student) {
+  store.editStudent(payload)
   toast.add({
     severity: 'success',
     summary: 'Success',
-    detail: 'Successfully updated subject!',
+    detail: 'Succesfully added subject!',
     life: 3000,
   })
   onClose()
@@ -27,23 +27,63 @@ function onSubmit(payload: Subject) {
 </script>
 
 <template>
-  <form class="flex flex-col gap-4" @submit.prevent="onSubmit(subject)">
+  <form class="flex flex-col gap-4" @submit.prevent="onSubmit(student)">
     <div class="flex flex-col gap-2">
-      <label>Subject Code</label>
-      <InputText required v-model="subject.code" />
+      <label>First Name</label>
+      <InputText required v-model="student.firstName" />
     </div>
     <div class="flex flex-col gap-2">
-      <label>Descriptive Title</label>
-      <InputText required v-model="subject.name" />
+      <label>Middle Name</label>
+      <InputText required v-model="student.middleName" />
     </div>
     <div class="flex flex-col gap-2">
-      <label>Units</label>
-      <InputText required v-model="subject.unit" :min="0" />
+      <label>Course</label>
+      <InputText required v-model="student.course" />
+    </div>
+    <div class="flex flex-col gap-2">
+      <label>Major</label>
+      <InputText required v-model="student.major" />
+    </div>
+    <div class="flex flex-col gap-2">
+      <label>Year</label>
+      <InputText required v-model="student.year" />
+    </div>
+    <div class="flex flex-col gap-2">
+      <label>Semester</label>
+      <InputText required v-model="student.semester" />
+    </div>
+    <div class="flex flex-col gap-2">
+      <label>Sex</label>
+      <InputText required v-model="student.sex" />
+    </div>
+    <div class="flex flex-col gap-2">
+      <label>Civil Status</label>
+      <InputText required v-model="student.civilStatus" />
+    </div>
+    <div class="flex flex-col gap-2">
+      <label>Date of Birth</label>
+      <InputText required v-model="student.birthDate" />
+    </div>
+    <div class="flex flex-col gap-2">
+      <label>Place of Birth</label>
+      <InputText required v-model="student.birthPlace" />
+    </div>
+    <div class="flex flex-col gap-2">
+      <label>Parent Name</label>
+      <InputText required v-model="student.parentName" />
+    </div>
+    <div class="flex flex-col gap-2">
+      <label>Parent Mobile Number</label>
+      <InputText required v-model="student.parentMobileNumber" />
+    </div>
+    <div class="flex flex-col gap-2">
+      <label>Parent Address</label>
+      <InputText required v-model="student.address" />
     </div>
 
     <div class="flex w-full gap-2 justify-end mt-4">
       <Button label="Cancel" class="p-button-text" @click="onClose" />
-      <Button label="Update" class="p-button-primary" type="submit" :loading="store.isLoading" />
+      <Button label="Save" class="p-button-primary" type="submit" :loading="store.isLoading" />
     </div>
   </form>
 </template>
--- src/stores/student.ts
+++ src/stores/student.ts
@@ -1,8 +1,10 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
 import type { Student } from '@/types/student'
+import { StudentRepository } from '@/repositories/studentRepository'
 
 export const useStudentStore = defineStore('student', () => {
+  const isLoading = ref(false)
   const students = ref<Student[]>([
     {
       uid: 'fsdf',
@@ -38,9 +40,59 @@ export const useStudentStore = defineStore('student', () => {
       },
     ]
   }
+  function addStudent(student: Student) {
+    students.value = [
+      {
+        uid: 'fsdf',
+        firstName: 'sdfsdf',
+        middleName: 'sfsdf',
+        lastName: 'sfdsd',
+        sex: 'fdsf',
+        civilStatus: 'sdfds',
+        studentMobileNumber: 'dsfsdf',
+        birthPlace: 'sfdsd',
+        parentName: 'sfsdfsddddddddd',
+        parentMobileNumber: '948239847823',
+        address: 'sdfsdf',
+        course: 'sdfsd',
+      },
+    ]
+    getStudents()
+    isLoading.value = false
+  }
 
+  function editStudent(student: Student) {
+    students.value = [
+      {
+        uid: 'fsdf',
+        firstName: 'sdfsdf',
+        middleName: 'sfsdf',
+        lastName: 'sfdsd',
+        sex: 'fdsf',
+        civilStatus: 'sdfds',
+        studentMobileNumber: 'dsfsdf',
+        birthPlace: 'sfdsd',
+        parentName: 'sfsdfsddddddddd',
+        parentMobileNumber: '948239847823',
+        address: 'sdfsdf',
+        course: 'sdfsd',
+      },
+    ]
+    getStudents()
+    isLoading.value = false
+  }
+
+  async function deleteStudent(uid: string) {
+    isLoading.value = true
+    await StudentRepository.destroyStudent(uid)
+    isLoading.value = false
+  }
   return {
     students,
+    isLoading,
+    addStudent,
     getStudents,
+    editStudent,
+    deleteStudent,
   }
 })
