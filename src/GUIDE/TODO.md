d2f8ad0a15c6d278f414cd2eced2838d845ffdbe
5kydriven <tagpuno619@gmail.com>
2025-06-08 11:08:22 +0800

    feat: define user roles and enhance user model with additional fields

---
+++ src/GUIDE/TODO.md
@@ -0,0 +1,6 @@
+### TODO'
+
+- [] Fix: routing guard
+- [] Fix: routing after enrollment
+- [] Fix: routing after sign in
+- [] Enhance: the code in auth to routing
--- src/types/user.d.ts
+++ src/types/user.d.ts
@@ -1,11 +1,18 @@
 import type { Student } from './student'
 
+export enum Role {
+  STUDENT = 'STUDENT',
+  ADMIN = 'ADMIN',
+}
+
 export interface User {
   uid?: string
   id?: string
   firstName?: string
   middleName?: string
   lastName?: string
+  name?: string
+  role?: Role
   email?: string
   password?: string
   student: Student
